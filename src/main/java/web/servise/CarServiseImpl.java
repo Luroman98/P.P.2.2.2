40a88e2097cb0e635ff91b9102140f2600b94ea5
Luroman98 <107890002+Luroman98@users.noreply.github.com>
2022-10-24 20:44:58 +0300

    Add files via upload

---
+++ src/main/java/web/servise/CarServiseImpl.java
@@ -0,0 +1,40 @@
+package web.servise;
+
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarServiseImpl implements CarServise {
+
+    @Override
+    public List<Car> getCars(int a) {
+        Car car = new Car("audi", "griin", 123);
+        Car car1 = new Car("toyota", "whyte", 1234);
+        Car car2 = new Car("cerroke", "blake", 12345);
+        Car car3 = new Car("jyguli", "yelou", 123456);
+        Car car4 = new Car("SHevrollet", "red", 1234567);
+        List<Car> cars = new ArrayList<>();
+        cars.add(car);
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        cars.add(car4);
+
+        if (a >= 5) {
+            a = 5;
+        }
+        if (a < 0) {
+            a = 0;
+        }
+        List<Car> cars1 = new ArrayList<>();
+
+        for(int i = 0; i < a; i++) {
+            cars1.add(cars.get(i));
+        }
+        return cars1;
+    }
+}
+
+
+
